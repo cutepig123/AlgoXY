812133c21b8bc6f0f08aedf23c6aa4e751c8d289
JSHe <JSHe>
2016-10-10 22:46:33 +0800

    update qsort

--- leetcode/qsort.cpp
+++ leetcode/qsort.cpp
@@ -49,10 +49,30 @@ int partition1(int *data, int left, int right) {
 	return -1;
 }
 
+//算法导论里面的方法，一次循环搞定
+int partition2(int *data, int left, int right) {
+	if (left < right) {
+		int pivot = data[right];
+		int i = left;
+		for (int j = i; j < right; j++)
+		{
+			if (data[j]<pivot)
+			{
+				swap(data[i], data[j]);
+				i++;
+			}
+		}
+		swap(data[i], data[right]);
+		return i;
+	}
+	assert(0);
+	return -1;
+}
+
 void testQsort() {
 	int data[] = { 3,1,5,4,9 };
 	int n = sizeof(data) / sizeof(data[0]);
 
-	g_partition1 = partition1;
+	g_partition1 = partition2;
 	qsort(data, 0, n - 1);
 }
